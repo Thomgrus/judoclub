4a47b519a79f9096d0e85a57f7753372857152a4
Thomas GRUSON <thomgrus@gmail.com>
2022-02-13 19:49:32 +0100

    feat(server): add QR Code scanner
    close #1

--- server/src/test/java/cloud/gruson/judoclub/server/ServerApplicationTest.java
+++ server/src/test/java/cloud/gruson/judoclub/server/ServerApplicationTest.java
@@ -1,15 +1,21 @@
 package cloud.gruson.judoclub.server;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-class ServerApplicationTests {
+class ServerApplicationTest {
+
+	@Value("${spring.flyway.enabled}")
+	boolean flywayMode;
 
 	@Test
 	void contextLoads() {
+		Assertions.assertFalse(flywayMode);
 	}
 
 }
